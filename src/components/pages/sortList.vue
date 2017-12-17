bf94de09b4253222a846cff23f92d9a48966ca11
kaker <614170417@qq.com>
2017-12-17 15:32:20 +0800

    二级分类

---
+++ src/components/pages/sortList.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<h2>二级分类页面</h2>
+		<ul>
+			<li v-for="item in mes" :key="item.age">{{item.age}}</li>
+		</ul>
+		<button @click="ck">点击</button>
+	</div>
+</template>
+
+<script>
+	//import Vue from "vue"
+	export default{
+		name:'car',
+		data(){
+			return {
+				mes:[{
+					name:'kaker',
+					age:18
+				},{
+					name:'eills',
+					age:30
+				}]
+			}
+		},
+		methods:{
+			ck(){
+				this.$set(this.mes ,0,{name:'angle',age:26})
+				console.log(this.mes)
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
